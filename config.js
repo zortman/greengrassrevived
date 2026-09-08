f8b63e37524e6d7b0d180fa5a8141566ea29e513
zortman <zortmanam3005@aguafria.org>
2026-09-08 14:04:16 -0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,28 @@
+// Global Configuration Database for Green Grass
+const GAME_DATABASE = {
+    1: {
+        title: "Find The Target",
+        description: ""
+    },
+    2: {
+        title: "Orbit Dodger",
+        description: ""
+    },
+    3: {
+        title: "Chrono Snatch",
+        description: ""
+    },
+    4: {
+        title: "Neon Fuse",
+        description: ""
+    },
+    5: {
+        title: "Piano Tiles",
+        description: ""
+    },
+    6: {
+        title: "Virus Snake",
+        description: ""
+    },
+    // You can add games up to 50 here following the exact same format
+};
